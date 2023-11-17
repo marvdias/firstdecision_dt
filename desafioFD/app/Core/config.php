18cebfaec1dee60935c33db39ad1cb71e8f59316
Marcus Dias <vmarcusv@gmail.com>
2023-11-17 16:19:16 -0300

    Update config.php

--- desafioFD/app/Core/config.php
+++ desafioFD/app/Core/config.php
@@ -1,29 +1,14 @@
 <?php
 
-if((empty($_SERVER['SERVER_NAME']) && php_sapi_name() == 'cli') || (!empty($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'localhost'))
-{
-	/** database config **/
-	define('DBTYPE', 'pgsql'); //mysql,pgsql
-	define('DBNAME', 'firstdecisiondt'); //db_name
-	define('DBHOST', 'pgsql'); //docker_container
-	define('DBUSER', 'postgres');
-	define('DBPASS', '123456');
-	define('DBDRIVER', '');
-	
-	define('ROOT', 'http://localhost/dt/desafioFD/public');
+/** database config **/
+define('DBTYPE', 'pgsql'); //mysql,pgsql
+define('DBNAME', 'firstdecisiondt'); //db_name
+define('DBHOST', 'pgsql'); //docker_container
+define('DBUSER', 'postgres');
+define('DBPASS', '123456');
+define('DBDRIVER', '');
 
-} else {
-	/** database config **/
-	define('DBTYPE', 'pgsql'); //mysql,pgsql
-	define('DBNAME', 'my_db');
-	define('DBHOST', 'localhost');
-	define('DBUSER', 'root');
-	define('DBPASS', '');
-	define('DBDRIVER', '');
-
-	define('ROOT', 'https://www.meuwebsite.com');
-
-}
+define('ROOT', 'http://localhost/dt/desafioFD/public'); //
 
 define('APP_NAME', "First Decision Desafio Tecnico");
 define('APP_DESC', "PHP Back-end + Postgresql");
